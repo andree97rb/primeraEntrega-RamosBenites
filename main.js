4a51cbcb94aa78b7b696d4fadabf3a42524e8009
Richard Andree Ramos Benites <richand_9702@outlook.com>
2023-03-18 17:41:11 -0500

    migración a código usando objetos

--- main.js
+++ main.js
@@ -1,130 +1,201 @@
+// Objetos y clases que se usan en el aplicativo
+
+class User {
+    constructor() {
+        this.names = "";
+        this.username = "";
+        this.age = 0;
+    }
+
+    displayWelcome() {
+        alert(`Bienvenido ${this.names.split(" ")[0]} a Minimarket Royal :D!!`);
+    }
+
+    displayUsername() {
+        alert(`Estimado ${this.names.split(" ")[0]} su username es: ${this.username}`);
+    }
+}
+
+class Product {
+    constructor(name, price, category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+
+    mostrarInfo() {
+        let info = "";
+        info += `${this.name}: ${this.price} soles`;
+        return info;
+    }
+}
+
+class Cart {
+    constructor() {
+        this.products = [];
+    }
+
+    addProduct(product) {
+        this.products.push(product);
+    }
+
+    removeProduct(product) {
+        const index = this.products.indexOf(product);
+        if (index > -1) {
+            this.products.splice(index, 1);
+        }
+    }
+
+    watchProductList() {
+        let listProducts = "";
+        this.products.forEach(
+            p => {
+                listProducts += `${p.name} ---> ${p.price} \n`;
+            }
+        );
+        return listProducts;
+    }
+
+    totalPrice() {
+        return this.products.reduce((total, product) => total + product.price, 0);
+    }
+}
+
+class Menu {
+    constructor(options) {
+        this.options = options;
+    }
+
+    mostrarMenu() {
+        let menuOption = `-------------------- Menú--------------------      [user: ${user.username}]\n`;
+        this.options.forEach((opcion, indice) => {
+            menuOption += `${indice + 1}. ${opcion} \n`;
+        });
+        return parseInt(prompt(menuOption));
+    }
+}
+
+class Minimarket {
+    constructor() {
+        this.mainMenu = new Menu(["Ver productos", "Comprar", "Configuración", "Salir"]);
+        this.cart = new Cart();
+        this.products = [
+            new Product("Arroz", 5, "Alimentos"),
+            new Product("Fideos", 8, "Alimentos"),
+            new Product("Harina", 7, "Alimentos"),
+            new Product("Azucar", 5, "Alimentos"),
+            new Product("Sal", 3, "Alimentos"),
+            new Product("Aceite", 10, "Alimentos"),
+            new Product("Leche", 6, "Alimentos"),
+            new Product("Pollo", 15, "Carnes"),
+            new Product("Carne de res", 25, "Carnes"),
+            new Product("Agua embotellada", 2, "Bebidas"),
+            new Product("Gaseosas", 3, "Bebidas"),
+            new Product("Cervezas", 12, "Bebidas"),
+            new Product("Vinos", 20, "Bebidas"),
+            new Product("Jabon en polvo", 8, "Limpieza"),
+            new Product("Detergente", 12, "Limpieza"),
+            new Product("Suavizante", 18, "Limpieza"),
+            new Product("Shampoo", 15, "Cuidado personal"),
+            new Product("Acondicionador", 12, "Cuidado personal"),
+            new Product("Pasta dental", 7, "Cuidado personal"),
+            new Product("Galletas", 4, "Snacks"),
+            new Product("Papas fritas", 5, "Snacks"),
+            new Product("Chicles", 2, "Snacks"),
+            new Product("Caramelos", 3, "Snacks"),
+        ];
+        this.categoriesMenu = new Menu(["Alimentos", "Carnes", "Bebidas", "Limpieza", "Cuidado personal", "Snacks", "Salir"]);
+        //this.productMenu = new Menu;
+        this.buyProducts = new Menu(["Agregar al carrito", "Quitar del carrito", "Comprar", "Salir"]);
+    }
+}
+
+
+const noEmptyString = (input, text, textAgain) => {
+    input = prompt(`${text} :`);
+    while (input.length === 0) {
+        input = prompt(`${textAgain} :`);
+    }
+    return input;
+}
+
+const productsByCategories = (minimarket, category) => {
+    let table = "";
+    minimarket.products.filter(product => product.category === category)
+        .forEach(
+            (product, index,) => {
+                table += `${index + 1}. ${product.name} ---> ${product.price} soles\n`
+            }
+        )
+    return table;
+}
+
+const minimarket = new Minimarket();
+
+const user = new User();
+
+const cart = new Cart();
+
+let age;
+
+age = parseInt(prompt("Ingrese su edad: "));
+
 let names = "";
 let username = "";
-let years = 0;
-let price1 = 5;
-let price2 = 8;
-let price3 = 7;
-let price5 = 12.40;
-let price6 = 9.90;
-let price7 = 26.50;
-let price8 = 3;
-let price9 = 28.90;
-let totalPrice = 0;
 let activeSubmenu = false;
 let activeSubmenu1 = false;
 
-const principalMenu = () => {
-    let menuOption = "Menu Principal\n\n";
-    menuOption += "1. Ver productos\n";
-    menuOption += "2. Comprar\n";
-    menuOption += "3. Configuración\n";
-    menuOption += "4. Salir\n";
-    return parseInt(prompt(menuOption));
-}
-
-const categoriesMenu = () => {
-    let menuOption2 = "Categorias de Prodcutos\n\n";
-    menuOption2 += "1. Alimentos\n";
-    menuOption2 += "2. Carnes\n";
-    menuOption2 += "3. Bebidas\n";
-    menuOption2 += "4. Limpieza\n";
-    menuOption2 += "5. Cuidado personal\n";
-    menuOption2 += "6. Snacks\n";
-    menuOption2 += "7. Salir\n";
-
-    return parseInt(prompt(`${menuOption2}\n ¿Qué categoría de productos buscas?: `));
-}
-
-const principalBuy = () => {
-    let menuOption = "Menu Comprar\n\n";
-    menuOption += "1. Comprar\n";
-    menuOption += "2. Quitar producto\n";
-    menuOption += "3. Salir\n";
-    return parseInt(prompt(menuOption));
-}
-
-
-
-const selectProduct = () => {
-    let product = parseInt(prompt("Ingrese el número del producto:"));
-    let price = 0;
-
-    switch (product) {
-        case 1: price = price1; break;
-        case 2: price = price2; break;
-        case 3: price = price3; break;
-        case 4: price = price1; break;
-        case 5: price = price1; break;
-        case 6: price = price5; break;
-        case 7: price = price2; break;
-        case 8: price = price6; break;
-        case 9: price = price7; break;
-        case 10: price = price1; break;
-        case 11.: price = price8; break;
-        case 12.: price = price7; break;
-        case 13: price = price7; break;
-        case 14: price = price1; break;
-        case 15: price = price2; break;
-        case 16: price = price7; break;
-        case 16: price = price7; break;
-        case 17: price = price9; break;
-        case 18: price = price7; break;
-        case 19: price = price7; break;
-        case 20: price = price5; break;
-        case 21: price = price6; break;
-        case 22: price = price6; break;
-        case 23: price = price2; break;
-    }
-    return price;
-}
-
-
-// MAIN
-
-years = parseInt(prompt("Ingrese su edad: "));
-
-if (years >= 18) {
-    while (names.length === 0) {
-        names = prompt("Ingrese su nombre completo: ");
-    }
-    alert("Bienvenido " + names.split(" ")[0] + " a Minimarket Royal :D!!");
-
-    while (username.length === 0) {
-        username = prompt("Ingrese su username: ");
-    }
-
-    alert("Estimado " + names.split(" ")[0] + " su username es: " + username);
+if (age >= 18) {
+    user.age = age;
+    names = noEmptyString(names, "Ingrese sus nombres completos", "Campo vacío, ingrese sus nombres completos correctamente");
+    username = noEmptyString(username, "Ingrese su username", "Campo vacío, ingrese su username correctamete");
+    user.names = names;
+    user.username = username;
+    user.displayWelcome();
+    user.displayUsername();
 
     let selectedOption;
 
     do {
-        selectedOption = principalMenu();
+        selectedOption = minimarket.mainMenu.mostrarMenu();
         switch (selectedOption) {
             case 1:
                 activeSubmenu = true;
                 while (activeSubmenu) {
-                    let category = categoriesMenu();
+                    let category = minimarket.categoriesMenu.mostrarMenu();
                     switch (category) {
                         case 1:
-                            alert(`1. Arroz: ${price1} soles/paquete\n2. Fideos: ${price2} soles/paquete\n3. Harina: ${price3} soles/paquete\n4. Azucar: ${price1} soles/paquete\n5. Sal: ${price1} soles/paquete\n6. Aceite: ${price5} soles/botella\n7. Leche: ${price2} soles/botella`);
+                            let table = "";
+                            table += productsByCategories(minimarket, "Alimentos");
+                            alert(table);
                             break;
                         case 2:
-                            alert(`8. Pollo: ${price6} soles 2.3kg\n9. Carne de res: ${price7} soles 0.7kg`);
+                            let table2 = "";
+                            table2 += productsByCategories(minimarket, "Carnes");
+                            alert(table2);
                             break;
                         case 3:
-                            alert(`10. Agua embotellada:  ${price1} soles/botella\n11. Gaseosas: ${price8} soles/lata\n12. Cervezas: ${price7} soles/caja de 6 unidades\n13. Vinos: ${price7} soles/unidad`);
+                            let table3 = "";
+                            table3 += productsByCategories(minimarket, "Bebidas");
+                            alert(table3);
                             break;
                         case 4:
-                            alert(`14. Jabon en polvo: ${price1} soles/unidad\n15. Detergente: ${price2} soles/botella\n16. Suavizante: ${price7} soles/botella\n`);
+                            let table4 = "";
+                            table4 += productsByCategories(minimarket, "Limpieza");
+                            alert(table4);
                             break;
                         case 5:
-                            alert(`17. Shampoo: ${price9} soles/botella\n18. Acondicionador: ${price7} soles/botella\n19. Pasta dental: ${price7} soles/tubo`);
+                            let table5 = "";
+                            table5 += productsByCategories(minimarket, "Cuidado personal");
+                            alert(table5);
                             break;
                         case 6:
-                            alert(`20. Galletas: ${price5} soles/paquete\n21. Papas fritas: ${price6} soles/bolsa\n22. Chicles: ${price6} soles/paquete\n23. Caramelos: ${price2} soles/bolsa`);
+                            let table6 = "";
+                            table6 += productsByCategories(minimarket, "Snacks");
+                            alert(table6);
                             break;
                         case 7:
-                            alert("Saliendo al menú principal");
+                            alert("Volviendo al menú principal");
                             activeSubmenu = false;
                             break;
                         default:
@@ -132,23 +203,32 @@ if (years >= 18) {
                             break;
                     }
                 }
-
                 break;
-
             case 2:
                 activeSubmenu1 = true;
                 while (activeSubmenu1) {
-                    let operationProduct = principalBuy();
+                    let operationProduct = minimarket.buyProducts.mostrarMenu();
                     switch (operationProduct) {
                         case 1:
-                            totalPrice += selectProduct();
-                            alert("total de precio es: " + totalPrice);
-                            break;
+                            productName = prompt("Ingrese el nombre del producto a comprar: ");
+                            minimarket.products.filter(product => product.name === productName)
+                                .forEach(product => cart.addProduct(product))
+                                ; break;
                         case 2:
-                            totalPrice -= selectProduct();
-                            alert("total de precio es: " + totalPrice);
-                            break;
+                            if (cart.products.length !== 0) {
+                                productName = prompt("Ingrese el nombre del producto a quitar del carro: ");
+                                minimarket.products.filter(product => product.name === productName)
+                                    .forEach(product => cart.removeProduct(product))
+                                    ; break;
+                            }else{
+                                alert("No hay más productos para eliminar");
+                            }
+
+
                         case 3:
+                            alert(`La lista de productos en su carro a comprar es la siguiente: \n -----Productos-----\n ${cart.watchProductList()} Total a pagar: ${cart.totalPrice()} soles`);
+                            break;
+                        case 4:
                             alert("Saliendo al menú principal");
                             activeSubmenu1 = false;
                             break;
@@ -159,27 +239,29 @@ if (years >= 18) {
                 }
                 break;
             case 3:
-                alert(`En el caso no quiera modificar algún dato, seleccione cancelar\n Sus Datos actuales son: \nNombres: ${names}\n Username: ${username}`);
-                let editNames = prompt("Edite su nombres completos: ");
+                alert(`En caso de no querer modificar algún dato, seleccione cancelar.\nSus Datos actuales son: \nNombres: ${user.names}\nUsername: ${user.username}`);
+                let editNames = prompt("Edite su nombre completo: ");
                 let editUsername = prompt("Edite su username: ");
 
-                if (editNames !== null) {
-                    names = editNames;
-                    alert(`Sus datos fueron actualizados:\n Nombres: ${names} \nUsername: ${username}`);
+                if (editNames !== null && editNames !== '') {
+                    user.names = editNames;
                 }
-                if (editUsername !== null) {
-                    username = editUsername;
-                    alert(`Sus datos fueron actualizados:\n Nombres: ${names} \nUsername: ${username}`);
+
+                if (editUsername !== null && editUsername !== '') {
+                    user.username = editUsername;
                 }
+
+                alert(`Sus datos fueron actualizados:\n Nombres: ${user.names} \nUsername: ${user.username}`);
                 break;
             case 4:
-                alert("Saliendo del menú...");
+                alert("Saliendo de la aplicación");
                 break;
             default:
-                console.log("Opción inválida. Por favor, elija una opción del menú.");
+                alert("Opción inválida. Por favor, elija una opción del menú.");
+                break;
         }
     } while (selectedOption !== 4);
+
 } else {
     alert("Lo sentimos, para comprar debe ser mayor de edad :(");
 }
-
